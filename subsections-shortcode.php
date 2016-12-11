1dcaee3365cae3a090dbfa27729cc159cd364cc2
Florian Heinle <launchpad@planet-tiax.de>
2016-12-11 14:21:28 +0100

    new loop

--- subsections-shortcode.php
+++ subsections-shortcode.php
@@ -11,29 +11,35 @@ License: GPL2
 
 function subsection_shortcode_init()
 {
-	function subsection_shortcode( $atts, $cnotent= null, tag='') {
+	function subsection_shortcode($atts = [], $content = null, $tag = '') {
 		$atts = array_change_key_case((array)$atts, CASE_LOWER);
 		$atts = shortcode_atts(
 			array(
 				'category' => 'about'
 			),
-			$atts
+			$atts,
+			$tags
 		);
-		$query_pages_with_category = array(
-			'post-type'	=> 'page',
-			'order'		=> 'ASC',
-			'orderby'	=> 'menu_order',
-			'taxonomy'	=> 'category',
-			'field'		=> 'slug',
-			'term'		=> $atts->category
+		$args = array(
+			'post_type'              => array( 'page' ),
+			'category_name'          => $atts->category,
+			'nopaging'               => true,
+			'order'                  => 'ASC',
+			'orderby'                => 'menu_order',
 		);
-		$category_pages = new WP_QUERY($query_pages_with_category);
-		echo "<ul>";
-		foreach ($category_pages as $category_page)
-		{
-			echo "<li>" . $category_page->post_title ."<li>";
+		$category_pages = new WP_QUERY($args);
+		if ($category_pages->have_posts()) {
+			$formatted_list_of_posts = "<ul>\n";
+			while ($category_pages->have_posts()) {
+				$category_pages->the_post();
+				$formatted_list_of_posts = $formatted_list_of_posts . "<li>Titel:" . get_the_title() . "</li>\n";
+			}
+			$formatted_list_of_posts = $formatted_list_of_posts . "</ul>\n";
+
+		} else {
+			$formatted_list_of_posts = "";
 		}
-		echo "</ul>";
+		return $formatted_list_of_posts;
 	}
 	add_shortcode( 'subsection', 'subsection_shortcode' );
 }
