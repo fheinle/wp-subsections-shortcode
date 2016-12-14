5d60421a1ebbed472c76a096429eab72640bef24
Florian Heinle <launchpad@planet-tiax.de>
2016-12-14 12:36:04 +0100

    formatting of different categories

--- subsections-shortcode.php
+++ subsections-shortcode.php
@@ -43,33 +43,47 @@ function subsection_shortcode_init()
 				$formatted_list_of_posts .= '<tr><td>' . $thumb . '</td>';
 				$formatted_list_of_posts .= '<td><h3>' . get_the_title($page) . '</h3>' . $content . '</td></tr>';
 			}
-			$formatted_list_of_posts .= '<table class="InternalTextBox"><tbody><tr><td colspan="2"><h3>' . get_the_title($page) . '</h3></td></tr>';
 			$formatted_list_of_posts .= '</tbody></table></div>';
+		} else {
+			foreach ( $category_pages as $page ) {
+				setup_postdata( $page );
+				$formatted_list_of_posts .= '<div class="' . $atts['picture_size'] . ' ' . $background_class . '">';
+				if ($atts['type'] == 'excerpt' || $atts['type'] == 'headline')
+				{
+					$formatted_list_of_posts .= '<a href="' . get_the_permalink($page) . '">';
+				}
+				if (has_post_thumbnail($page)) {
+					$formatted_list_of_posts .= get_the_post_thumbnail($page, $atts['picture_size']);
+				}
+				$formatted_list_of_posts .= '<table class="InternalTextBox"><tbody><tr><td colspan="2"><h3>' . get_the_title($page) . '</h3></td></tr>';
 
-			switch ($atts['type']) {
-			case 'excerpt':
-				$formatted_list_of_posts .= '<tr><td class="ProfessionText">' . get_the_excerpt($page) . '</td></tr>';
-				break;
-			case 'content':
-				$content = apply_filters('the_content', get_the_content($page));
-				$formatted_list_of_posts .= '<tr><td>' . $content . '</td></tr>';
-				break;
-			case 'link':
-				$formatted_list_of_posts .= '<tr><td><a href="' . get_the_excerpt($page) . '">' . get_the_content($page) . '</td></tr>';
-				break;
-			} /* TODO: Add custom symbol */
+				switch ($atts['type']) {
+				case 'headline':
+					break;
+				case 'excerpt':
+					$formatted_list_of_posts .= '<tr><td class="ProfessionText">' . get_the_excerpt($page) . '</td></tr>';
+					break;
+				case 'content':
+					$content = apply_filters('the_content', get_the_content($page));
+					$formatted_list_of_posts .= '<tr><td>' . $content . '</td></tr>';
+					break;
+				case 'link':
+					$formatted_list_of_posts .= '<tr><td><a href="' . get_the_excerpt($page) . '">' . get_the_content($page) . '</td></tr>';
+					break;
+				} /* TODO: Add custom symbol */
 
-			$formatted_list_of_posts .= '</table>';
-			if ($atts['type'] == 'excerpt') {
-				$formatted_list_of_posts .= '</a>';
+				$formatted_list_of_posts .= '</table>';
+				if ($atts['type'] == 'excerpt' || $atts['type'] == 'headline') {
+					$formatted_list_of_posts .= '</a>';
+				}
+				$formatted_list_of_posts .= '</div>';
 			}
-			$formatted_list_of_posts .= '</div>';
 		}
 		wp_reset_postdata();
 		$formatted_list_of_posts .= '<div class="FloatClearDiv"></div>';
 		return $formatted_list_of_posts;
+		}
+		add_shortcode( 'subsection', 'subsection_shortcode' );
 	}
-	add_shortcode( 'subsection', 'subsection_shortcode' );
-}
-add_action('init', 'subsection_shortcode_init');
+	add_action('init', 'subsection_shortcode_init');
 ?>
